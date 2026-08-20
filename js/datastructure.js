f05d6c96180354d877bf359b8abe66be6c54c23b
Juhi3921 <anandikarjuhi@gmail.com>
2026-08-20 22:40:19 +0530

    game logic around circle added

---
+++ js/datastructure.js
@@ -0,0 +1,27 @@
+
+var Circle = function (r, circleClass, x, y) {
+    
+    this.id;
+    this.r = r;
+    this.x = x;
+    this.y = y;
+    this.t;
+    this.htmlCircle;
+    this.circleClass = circleClass;
+}
+var Me = function (r, x, y, meClass) {
+    this.r = r;
+    this.x = x;
+    this.y = y;
+    this.velocity = 2.5
+    this.direction = 1
+    this.currentCircle;
+    this.htmlMe;
+    this.meClass = meClass;
+}
+var Game = function () {
+    this.collectibleElements = [];
+    this.collectedElements = [];
+    this.circleArray = []
+    this.lives = 3;
+}
